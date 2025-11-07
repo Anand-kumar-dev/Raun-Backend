5d76da97cfb15b4b677dc7a2adf41b347abc465b
Anand Kumar <anand024601@gmail.com>
2025-11-07 20:10:13 +0530

    added zerodha callback logic

--- src/controllers/zerodha.controller.js
+++ src/controllers/zerodha.controller.js
@@ -1,6 +1,7 @@
 import { KiteConnect } from "kiteconnect";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import kite from "../services/kite.service.js";
 
 dotenv.config();
 
@@ -8,12 +9,49 @@ dotenv.config();
 
 export const kitelogin = async (req, res) => {
 
-    const stateToken = jwt.sign({ Id: req.user}, process.env.JWT_SECRET, {
+    const stateToken = jwt.sign({ Id: req.user }, process.env.JWT_SECRET, {
         expiresIn: "5m"
     });
 
     const api_key = process.env.ZERODHA_API_KEY;
     const redirectParams = encodeURIComponent(`state=${stateToken}`);
-    res.redirect(`https://kite.zerodha.com/connect/login?v=3&api_key=${api_key}&redirect_params=${redirectParams}`);
+    res
+        .status(200)
+        .json({ url: `https://kite.zerodha.com/connect/login?v=3&api_key=${api_key}&redirect_params=${redirectParams}` });
 
 }
+
+
+
+export const kiteCallback = async (req, res) => {
+    try {
+
+         const request_token = req.query.request_token;
+        const stateToken = req.query.state; 
+
+
+        if (!request_token) throw new ApiError("Request token is required in kiteconnect", 400);
+        if (!stateToken) throw new ApiError("Missing state token in kiteconnect", 400);
+
+
+        const decoded = jwt.verify(stateToken, process.env.JWT_SECRET);
+        if (!decoded) return res.status(401).json({ message: "Invalid or expired state token" });
+
+        const response = await kite.generateSession(request_token, process.env.ZERODHA_API_SECRET);
+        const Kiteaccess_token = response.access_token;
+        kite.setAccessToken(Kiteaccess_token);
+
+        try {
+            
+            const profile = await kite.getProfile();
+            res.json(profile);
+        } catch (err) {
+            res.send(err);
+        }
+    } catch (error) {
+        res.status(400).json({ 
+            mes: "Error in kite callback",
+             error: error.message });
+    }
+
+};
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,13 @@
 import Router from "express"
 import { verifyToken } from "../middleware/auth.middleware.js"
-import { kitelogin } from "../controllers/zerodha.controller.js"
+import { kiteCallback, kitelogin } from "../controllers/zerodha.controller.js"
 
 
 const router = Router()
 
 router.get("/kitelogin" , verifyToken ,kitelogin);
 
+router.get("/callback",kiteCallback)
 
 
 export default router
